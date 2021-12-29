9da641f428186182261c8c98ad19c5e72342b7ed
Daniel <contact@jiftoo.dev>
2021-12-29 23:59:34 +0300

    New deploy

--- deploy-gh.cmd
+++ deploy-gh.cmd
@@ -2,7 +2,7 @@ npm run build && ^
 git add . && ^
 git commit -m "New deploy" && ^
 git push && ^
-git checkout master && ^ 
-git subtree split --prefix dist -b gh-pages
-git push -f origin gh-pages:gh-pages
+git checkout master && ^
+git subtree split --prefix dist -b gh-pages && ^
+git push -f origin gh-pages:gh-pages && ^
 git branch -D gh-pages
