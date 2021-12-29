eddbcb92e268337bd3bad041914e0a014d999f49
Daniel <contact@jiftoo.dev>
2021-12-29 23:57:56 +0300

    Updated deploy-gh

--- deploy-gh.cmd
+++ deploy-gh.cmd
@@ -6,4 +6,4 @@ git push && ^
 git checkout master && ^ 
 git subtree split --prefix dist -b gh-pages && ^
 git push -f origin gh-pages:gh-pages && ^
-git branch -D gh-pages && ^
+git branch -D gh-pages
