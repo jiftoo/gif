9c0884be9d72eea5ff31774be26aec34cdb7049a
Daniel <contact@jiftoo.dev>
2022-01-07 19:21:47 +0300

    Added partial manifest.json; Reduced size of backgroundnoise.png

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	base: "/gif/",
-    build: {
-        outDir: "dist/gif"
-    },
+	build: {
+		outDir: "dist/gif",
+	},
 	server: {
 		open: true,
 	},
