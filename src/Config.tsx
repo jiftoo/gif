c07525f625a470ebba456e6f3cdb2ec2e3d977ae
Daniel <contact@jiftoo.dev>
2022-04-21 13:55:15 +0300

    Forgot to change api links

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,8 +1,8 @@
 export default {
-	// LITTERBOX_API_URL: "https://jiftoo.dev/gif/litterbox",
-	LITTERBOX_API_URL: "http://localhost:8002/gif/litterbox",
-	// CORS_PROXY_URL: "https://jiftoo.dev/gif/cors",
-	CORS_PROXY_URL: "http://localhost:8002/gif/cors",
+	LITTERBOX_API_URL: "https://jiftoo.dev/gif/litterbox",
+	// LITTERBOX_API_URL: "http://localhost:8002/gif/litterbox",
+	CORS_PROXY_URL: "https://jiftoo.dev/gif/cors",
+	// CORS_PROXY_URL: "http://localhost:8002/gif/cors",
 	DEFAULT_WORKERS: 2,
 	GIF_QUALITY: 13, // 1 - 30? (lower is better)
 	CAPTION_FONT_SIZE_MIN: 0.25,
